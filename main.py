f6bb59150c294b925afe0a01ca9bd79c4e11ee44
23z23x <1zach2summers@gmail.com>
2022-07-08 08:48:25 -0500

    Added README, Main

---
+++ main.py
@@ -0,0 +1,29 @@
+#Control Variables
+
+#Controls the state of the program
+option = -1
+
+#main
+while(option != 4):
+
+    print("1) New Board")
+    print("2) Load Board")
+    print("3) Create Entity")
+    print("4) Exit")
+    option = input()
+
+    if(option == 1):
+
+        #Create New Board
+
+    if(option == 2):
+
+        #Load Board From File
+
+    if(option == 3):
+
+        #Create Entity
+
+    if(option == 4):
+        
+        #Exit
